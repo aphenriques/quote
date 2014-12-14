c23bc72329db59c19473b4ba6cee888d14810b8d
Andre Henriques <aphenriques@outlook.com>
2014-12-14 13:44:20 -0200

    macosx .so -> dylib; makefile improvments

--- common.mk
+++ common.mk
@@ -1,9 +1,15 @@
-INSTALL_TOP= /usr/local
-INSTALL_INC= $(INSTALL_TOP)/include/quote
-INSTALL_LIB= $(INSTALL_TOP)/lib
+INSTALL_TOP:=/usr/local
+INSTALL_INC:=$(INSTALL_TOP)/include/quote
+INSTALL_LIB:=$(INSTALL_TOP)/lib
+
+ifeq ($(shell uname -s),Darwin)
+SHARED_LIB_EXTENSION:=dylib
+else
+SHARED_LIB_EXTENSION:=so
+endif
 
 PROJECT_STATIC_LIB_NAME:=libquote.a
-PROJECT_SHARED_LIB_NAME:=libquote.so
+PROJECT_SHARED_LIB_NAME:=libquote.$(SHARED_LIB_EXTENSION)
 
 CXXFLAGS:=-std=c++11 -O3 -I/usr/local/include
 LDFLAGS:=-L/usr/local/lib -L/usr/lib/x86_64-linux-gnu/ -L/usr/lib
