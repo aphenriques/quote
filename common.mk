566090bdc477e247dcd293c70e723debc8fb1313
Andre Henriques <aphenriques@outlook.com>
2016-11-06 14:30:21 -0200

    up to date exception submodule; compiler warnings enabled

--- common.mk
+++ common.mk
@@ -11,6 +11,8 @@ endif
 PROJECT_STATIC_LIB_NAME:=libquote.a
 PROJECT_SHARED_LIB_NAME:=libquote.$(SHARED_LIB_EXTENSION)
 
-CXXFLAGS:=-std=c++11 -O3 -I/usr/local/include
+# TODO with c++17: remove -Wno-unused-parameter and use [[maybe_unused]] attribute
+# -Wweak-vtables is a clang feature (gcc ignores it)
+CXXFLAGS:=-std=c++11 -O3 -I/usr/local/include -Wall -Wextra -Wshadow -Wweak-vtables -Wnon-virtual-dtor -Wno-missing-braces -Wno-unused-parameter -pedantic
 LDFLAGS:=-L/usr/local/lib -L/usr/lib/x86_64-linux-gnu/ -L/usr/lib
 PROJECT_LDLIBS:=-lcurl
