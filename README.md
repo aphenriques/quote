59626e53f0dec203f0f8387d099711cc51c5951f
aphenriques <aphenriques@outlook.com>
2017-05-04 18:22:00 +0000

    improved makefiles; fixed file access permissions

--- README.md
+++ README.md
@@ -112,7 +112,7 @@ See doc/html/index.html for the doxygen generated `quote` API reference.
 
 # License
 
-Copyright (C) 2013, 2014, 2015, 2016  André Pereira Henriques.
+Copyright (C) 2013, 2014, 2015, 2016, 2017  André Pereira Henriques.
 
 quote is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
--- common.mk
+++ common.mk
@@ -1,21 +1,26 @@
-INSTALL_TOP:=/usr/local
-INSTALL_INC:=$(INSTALL_TOP)/include/quote
-INSTALL_LIB:=$(INSTALL_TOP)/lib
+PROJECT:=quote
 
+PROJECT_LIB_DIR:=src
+PROJECT_BIN_DIR:=samples
+PROJECT_STATIC_LIB:=lib$(PROJECT).a
+
+# some variables require PROJECT_ROOT_DIR definition. That's why = is used instead of := for their definition
+PROJECT_INCLUDE_DIRS=$(PROJECT_ROOT_DIR)/$(PROJECT_LIB_DIR)
+PROJECT_LDLIBS=$(PROJECT_ROOT_DIR)/$(PROJECT_LIB_DIR)/$(PROJECT_STATIC_LIB) -lcurl
+PROJECT_SYSTEM_INCLUDE_DIRS:=/usr/local/include
+PROJECT_LIB_DIRS:=/usr/local/lib
 # TODO with c++17: remove -Wno-unused-parameter and -Wno-unused-but-set-parameter and use [[maybe_unused]] attribute
-CXXFLAGS:=-std=c++11 -O3 -I/usr/local/include -Wall -Wextra -Wshadow -Wnon-virtual-dtor -Wno-missing-braces -Wno-unused-parameter -pedantic
-LDFLAGS:=-L/usr/local/lib -L/usr/lib/x86_64-linux-gnu/ -L/usr/lib
-
-PROJECT_LDLIBS:=-lcurl
+PROJECT_CXXFLAGS:=-std=c++14 -O0 -Wall -Wextra -Wshadow -Wnon-virtual-dtor -Wno-missing-braces -Wno-unused-parameter -pedantic
 
 ifeq ($(shell uname -s),Darwin)
 # -Wweak-vtables is a clang feature
-CXXFLAGS+=-Wweak-vtables
+PROJECT_CXXFLAGS+=-Wweak-vtables
 SHARED_LIB_EXTENSION:=dylib
+PROJECT_LDFLAGS:=-undefined dynamic_lookup
 else
-CXXFLAGS+=-Wno-unused-but-set-parameter
+PROJECT_CXXFLAGS+=-Wno-unused-but-set-parameter
 SHARED_LIB_EXTENSION:=so
+PROJECT_LDFLAGS:=
 endif
 
-PROJECT_STATIC_LIB_NAME:=libquote.a
-PROJECT_SHARED_LIB_NAME:=libquote.$(SHARED_LIB_EXTENSION)
+PROJECT_SHARED_LIB:=lib$(PROJECT).$(SHARED_LIB_EXTENSION)
